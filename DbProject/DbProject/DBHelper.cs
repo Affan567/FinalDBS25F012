bd8644e531a99ee152067fb0935c597250d03aa6
Muhammad-Saad28 <msaad23806@gmail.com>
2025-05-11 19:21:25 +0500

    Maaf kr yrr

---
+++ DbProject/DbProject/DBHelper.cs
@@ -0,0 +1,130 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+
+namespace DbProject
+{
+    public static class DBHelper
+    {
+        private static string connectionString = "server=localhost;user id=root;password=Saad@12345;database=finalproject;";
+
+        public static DataTable ExecuteSelect(string query, params MySqlParameter[] parameters)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                using (MySqlDataAdapter adapter = new MySqlDataAdapter(query, conn))
+                {
+                    foreach (var param in parameters)
+                    {
+                        adapter.SelectCommand.Parameters.Add(param);
+                    }
+
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+                    return dt;
+                }
+            }
+        }
+
+        public static DataTable executeSelect(string query, MySqlParameter param)
+        {
+            using (var conn = new MySqlConnection(connectionString))
+            {
+                using (var cmd = new MySqlCommand(query, conn))
+                {
+                    cmd.Parameters.Add(param);
+                    conn.Open();
+                    using (var adapter = new MySqlDataAdapter(cmd))
+                    {
+                        DataTable dt = new DataTable();
+                        adapter.Fill(dt);
+                        return dt;
+                    }
+                }
+            }
+        }
+        public static int ExecuteNonQuery(string query, params MySqlParameter[] parameters)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                {
+                    foreach (var param in parameters)
+                    {
+                        cmd.Parameters.Add(param);
+                    }
+                    return cmd.ExecuteNonQuery();
+                }
+            }
+        }
+        public static void executeNonQuery(string query, MySqlParameter[] parameters)
+        {
+            using (var conn = new MySqlConnection(connectionString))
+            {
+                using (var cmd = new MySqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddRange(parameters);
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+        public static MySqlDataReader ExecuteReader(string query, params MySqlParameter[] parameters)
+        {
+            var conn = new MySqlConnection(connectionString);
+            var cmd = new MySqlCommand(query, conn);
+            foreach (var param in parameters)
+            {
+                cmd.Parameters.Add(param);
+            }
+            conn.Open();
+            return cmd.ExecuteReader(System.Data.CommandBehavior.CloseConnection);
+        }
+
+        public static string ExecuteScalar(string query, params MySqlParameter[] parameters)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                {
+                    if (parameters != null)
+                        cmd.Parameters.AddRange(parameters);
+
+                    conn.Open();
+                    object result = cmd.ExecuteScalar();
+                    return result != null ? result.ToString() : "0";
+                }
+            }
+        }
+
+        public static DataTable GetData(string query, params MySqlParameter[] parameters)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            using (MySqlCommand cmd = new MySqlCommand(query, conn))
+            {
+                if (parameters != null)
+                    cmd.Parameters.AddRange(parameters);
+
+                MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                return dt;
+            }
+        }
+
+        public static int ExecuteQuery(string query, params MySqlParameter[] parameters)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            using (MySqlCommand cmd = new MySqlCommand(query, conn))
+            {
+                conn.Open();
+                if (parameters != null)
+                    cmd.Parameters.AddRange(parameters);
+
+                return cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
+
---
+++ DbProject/DbProject/OpenForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DbProject
+{
+    internal class OpenForm
+    {
+        public void openForm(Form currentForm, Form newForm)
+        {
+            newForm.Size = currentForm.Size;
+            newForm.StartPosition = FormStartPosition.Manual;
+            newForm.Location = currentForm.Location;
+            newForm.Show();
+            currentForm.Hide();
+        }
+    }
+}
